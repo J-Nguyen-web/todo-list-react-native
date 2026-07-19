06178393e8ca40a2c86b9b06b9c1d4ef390ed67b
J-Nguyen-web <map.genshin.impacts@gmail.com>
2026-07-19 19:50:21 +0300

    add dummy data & design fix

--- src/components/CardTask.jsx
+++ src/components/CardTask.jsx
@@ -1,23 +1,45 @@
-import { Ionicons } from "@expo/vector-icons";
+import { Entypo, Ionicons, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, View, Text } from "react-native";
+import { CATEGORY_CONFIG } from "../constants/categories.js";
 
 export default function CardTask({
     title,
     type,
-    completed
+    completed,
+    time
 }) {
+
+    const category = CATEGORY_CONFIG[type] ?? CATEGORY_CONFIG.personal
+    console.log(CATEGORY_CONFIG)
+    console.log(CATEGORY_CONFIG[type])
+    console.log(CATEGORY_CONFIG.personal)
+
     return (    
         <View style={styles.cardContainer}>
-            <Ionicons
-                name={completed ? "checkmark-circle" : "ellipse-outline"}
-                size={28}
-                color={completed ? "#50a353" : "#999"}
-            />
-            {/* todo icons for categories based on type value */}
-            <View>
-                <Text>{title}</Text>
-                <Text>{type}</Text>
+
+            <View style={styles.leftPart}>
+                <Ionicons
+                    name={completed ? "checkmark-circle" : "ellipse-outline"}
+                    size={28}
+                    color={completed ? "#50a353" : "#999"}
+                />
+                {/* todo icons for categories based on type value */}
+                <MaterialCommunityIcons
+                    name={category.icon}
+                    size={25}
+                    color={category.color}
+                />
+                <View style={styles.titleContainer}>
+                    <Text style={{flexWrap: 'wrap'}}>{title}</Text>
+                    <Text style={{ color: category.color}}>{type}</Text>
+                </View>
             </View>
+            <View style={styles.rightPart}>
+                <Text>{time}</Text>
+                <Entypo name="chevron-small-down" size={25} color='gray' />
+                <MaterialIcons name="drag-indicator" size={25} color='gray' />
+            </View>
+
         </View>
 
     );
@@ -25,7 +47,10 @@ export default function CardTask({
 
 const styles = StyleSheet.create({
     cardContainer: {
+        flex:1,
         flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         backgroundColor: '#fff',
         borderRadius: 18,
         padding: 16,
@@ -42,5 +67,25 @@ const styles = StyleSheet.create({
 
         // android (combines all above)
         elevation: 3
+    },
+
+    leftPart: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        width: '70%',
+        gap: '3%',
+    },
+
+    titleContainer: {
+        flex:1,
+        marginRight: '3%',
+    },
+    rightPart: {
+        width: '30%',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        alignSelf: 'flex-start',
+        gap: '3%',
     }
 })
---
+++ src/constants/categories.js
@@ -0,0 +1,28 @@
+export const CATEGORY_CONFIG = {
+    Work: {
+        icon: 'briefcase',
+        color: '#6366f1',
+        background: '#eef2ff',
+    },
+    Shopping: {
+        icon: 'cart',
+        color: '#14b8a6',
+        background: '#ECFDF5',
+    },
+    personel: {
+        icon: 'account',
+        color: '#7948ec',
+        background: '#e7ddfc',
+    },
+    Study: {
+        icon: 'book-open-page-variant',
+        color: '#f5950b',
+        background: '#FFFBEB',
+    },
+    Health: {
+        icon: 'heart',
+        color: '#ec4899',
+        background: '#fdf2f8',
+    },
+    
+}
